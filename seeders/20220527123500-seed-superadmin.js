28268fb793a909a2bdeef23b92b60fa478fc649e
alreagon <alpharajasa@gmail.com>
2024-05-28 17:10:03 +0700

    update seeder

---
+++ seeders/20220527123500-seed-superadmin.js
@@ -0,0 +1,25 @@
+"use strict";
+const bcrypt = require("bcrypt");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const hashPassword = await bcrypt.hash("123456", 10);
+    await queryInterface.bulkInsert("Superadmins", [
+      {
+        name: "Joko",
+        email: "superadmin@gmail.com",
+        password: hashPassword,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete(
+      "Superadmins",
+      { email: "superadmin@gmail.com" },
+      {}
+    );
+  },
+};
